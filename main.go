2c1b8ef7947ddd7bde27ce9f20e633abf9388e99
Jérémy Gendre <jgendre@media-sante.com>
2021-01-27 14:41:17 +0100

    add range function

--- main.go
+++ main.go
@@ -18,6 +18,37 @@ func main() {
     slicesFunc()
     fmt.Println("=== MAP ===")
     mapFunc()
+    fmt.Println("=== RANGE ===")
+    rangeFunc()
+}
+
+func rangeFunc() {
+
+    nums := []int{2, 3, 4}
+    sum := 0
+    for _, num := range nums {
+        sum += num
+    }
+    fmt.Println("sum:", sum)
+
+    for i, num := range nums {
+        if num == 3 {
+            fmt.Println("index:", i)
+        }
+    }
+
+    kvs := map[string]string{"a": "apple", "b": "banana"}
+    for k, v := range kvs {
+        fmt.Printf("%s -> %s\n", k, v)
+    }
+
+    for k := range kvs {
+        fmt.Println("key:", k)
+    }
+
+    for i, c := range "go" {
+        fmt.Println(i, c)
+    }
 }
 
 func mapFunc() {
