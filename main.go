58948ce37c732a38e304827888890c3b3514745a
Jérémy Gendre <jgendre@media-sante.com>
2021-01-27 14:29:49 +0100

    add map Function

--- main.go
+++ main.go
@@ -16,6 +16,32 @@ func main() {
     arrayFunc()
     fmt.Println("=== SLICE ===")
     slicesFunc()
+    fmt.Println("=== MAP ===")
+    mapFunc()
+}
+
+func mapFunc() {
+
+    m := make(map[string]int)
+
+    m["k1"] = 7
+    m["k2"] = 13
+
+    fmt.Println("map:", m)
+
+    v1 := m["k1"]
+    fmt.Println("v1: ", v1)
+
+    fmt.Println("len:", len(m))
+
+    delete(m, "k2")
+    fmt.Println("map:", m)
+
+    _, prs := m["k2"]
+    fmt.Println("prs:", prs)
+
+    n := map[string]int{"foo": 1, "bar": 2}
+    fmt.Println("map:", n)
 }
 
 func slicesFunc() {
