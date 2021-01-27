989543ce91da2723cdd342ad756dda49c7ca204f
Jérémy Gendre <jgendre@media-sante.com>
2021-01-27 14:44:34 +0100

    typo

--- main.go
+++ main.go
@@ -26,13 +26,13 @@ func rangeFunc() {
 
     nums := []int{2, 3, 4}
     sum := 0
-    for _, num := range nums {
-        sum += num
+    for _, value := range nums {
+        sum += value
     }
     fmt.Println("sum:", sum)
 
-    for i, num := range nums {
-        if num == 3 {
+    for i, value := range nums {
+        if value == 3 {
             fmt.Println("index:", i)
         }
     }
